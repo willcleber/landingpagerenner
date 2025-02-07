b310c060c5c2825c5bc5913dcd836a94ac7e033f
willcleber <willcleber.caetano@gmail.com>
2025-02-07 13:19:11 -0300

    algumas alteraçãos de responsividade

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ document.querySelectorAll('section').forEach(section => {
   document.querySelectorAll('.section').forEach(section => {
     section.classList.add('fade-in');
   });
+
+
+// Captura o evento de envio do formulário
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault(); // Previne o comportamento padrão do formulário
+
+  // Captura os valores dos campos
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const message = document.getElementById('message').value;
+
+  // Cria a mensagem formatada para o WhatsApp
+  const whatsappMessage = `Olá, meu nome é ${name}. Meu email é ${email} e meu telefone é ${phone}. Minha mensagem é: ${message}`;
+
+  // Codifica a mensagem para URL
+  const encodedMessage = encodeURIComponent(whatsappMessage);
+
+  // Número de telefone do WhatsApp
+  const phoneNumber = '+5562992727449';
+
+  // Redireciona para o WhatsApp
+  window.open(`https://wa.me/${phoneNumber}?text=${encodedMessage}`, '_blank');
+});
